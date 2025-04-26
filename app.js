5a0c8bbef5ae71f0f1a07d3c14a9c67896a59dbc
João Marcelo Gomes Correia <61568775+jmgcorreia99@users.noreply.github.com>
2025-04-26 11:41:02 -0300

    Update app.js
    corrigindo velocidade 2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 
-   responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1});
+   responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.2});
 
    
 }
